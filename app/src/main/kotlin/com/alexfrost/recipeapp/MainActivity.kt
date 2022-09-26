896a71bec84fb284418616f3d335b55f0051e424
Alex <alexfrost4u@gmail.com>
2022-09-26 14:56:18 +0500

    refactor: common ui module gradle file
    Move activity main xml file to app module to avoid its' exposure
    
    Refs: #2

--- app/src/main/kotlin/com/alexfrost/recipeapp/MainActivity.kt
+++ app/src/main/kotlin/com/alexfrost/recipeapp/MainActivity.kt
@@ -2,11 +2,10 @@ package com.alexfrost.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.alexfrost.core.ui.R as CommonRes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(CommonRes.layout.activity_main)
+        setContentView(R.layout.activity_main)
     }
 }
